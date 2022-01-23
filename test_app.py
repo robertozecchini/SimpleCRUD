328d7533f870745bec6cd547d10034d99124b679
rz <rz@sanchopanza>
2022-01-23 08:52:33 +0100

    feat: add a simple test

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import unittest
+
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.ctx = app.app_context()
+        self.ctx.push()
+        self.client = app.test_client()
+
+    def tearDown(self):
+        self.ctx.pop()
+
+    def test_home(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
